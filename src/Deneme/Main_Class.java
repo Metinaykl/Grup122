6ce6241c309679405ee3e4fa895967b4a6fbd703
Metin Aykul <metinaykul1@gmail.com>
2023-01-30 23:40:30 +0300

    benimde tamam

--- src/Deneme/Main_Class.java
+++ src/Deneme/Main_Class.java
@@ -4,5 +4,6 @@ public class Main_Class {
     public static void main(String[] args) {
 
         // isim ve soyisimlerinizi kendi branchinde sout yapsın
+        System.out.println("Metin Aykul");
     }
 }
